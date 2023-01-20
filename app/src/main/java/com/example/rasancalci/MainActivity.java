55efacdfef2fba38bf81bea9f97b498399de014a
Singularity Dynamics <rohitkverma1998@gmail.com>
2023-01-20 22:21:15 +0530

    Having_Setting_Feature

--- app/src/main/java/com/example/rasancalci/MainActivity.java
+++ app/src/main/java/com/example/rasancalci/MainActivity.java
@@ -2,16 +2,39 @@ package com.example.rasancalci;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private Spinner spinnerTextSize;
+    private Spinner spinner;
+
+    private Button button;
+    ////////////////////////////////////////////////////////////////////////
+
+    private float Saved_Rice_PP;
+    private float Saved_Wheat_PP;
+    private float Saved_Deduct_Rice_PP;
+    private float Saved_Deduct_Wheat_PP;
+    private float Saved_Free_Rice_PP;
+    private float Saved_Free_Wheat_PP;
+    private float Saved_Deduct_Free_Rice_PP;
+    private float Saved_Deduct_Free_Wheat_PP;
+    private float Saved_Take_Rice_Price_Pkg;
+    private float Saved_Take_Wheat_Price_Pkg;
+    private float Saved_Rice_Price_Pkg;
+    private float Saved_Wheat_Price_Pkg;
+
+    ///////////////////////////////////////////////////////////////////////
 
     private TextView View_Rice_Total;
     private TextView View_Wheat_Total;
@@ -24,15 +47,81 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        spinnerTextSize = findViewById(R.id.spinnerTextSize);
 
-        spinnerTextSize.setOnItemSelectedListener(this);
+//        spinnerTextSize = findViewById(R.id.spinnerTextSize);
+//
+//        spinnerTextSize.setOnItemSelectedListener(this);
+//
+//        String[] textSizes = getResources().getStringArray(R.array.font_sizes);
+//        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item,textSizes);
+//
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinnerTextSize.setAdapter(adapter);
+
+
+
+
+
+
+
+        spinner = (Spinner) findViewById(R.id.spinnerTextSize);
 
         String[] textSizes = getResources().getStringArray(R.array.font_sizes);
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item,textSizes);
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item,textSizes);
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerTextSize.setAdapter(adapter);
+        spinner.setAdapter(adapter);
+
+        // Retrieve spinner position from sharedpreferences
+        SharedPreferences sharedPref = getSharedPreferences("Saved", MODE_PRIVATE);
+        int spinnerValue = sharedPref.getInt("userChoiceSpinner",-1);
+        if(spinnerValue != -1)
+            // set the value of the spinner
+            spinner.setSelection(spinnerValue);
+
+        spinner.setOnItemSelectedListener(this);
+
+
+
+
+
+
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSetting();
+            }
+        });
+
+
+        SharedPreferences sh = getSharedPreferences("MySharedPref",MODE_PRIVATE);
+        ///////////////////////////////////////////////////////////////////////////////////////////
+
+        Saved_Rice_PP = sh.getFloat("Saved_Rice_PP",0);
+        Saved_Wheat_PP = sh.getFloat("Saved_Wheat_PP",0);
+        Saved_Deduct_Rice_PP = sh.getFloat("Saved_Deduct_Rice_PP",0);
+        Saved_Deduct_Wheat_PP = sh.getFloat("Saved_Deduct_Wheat_PP",0);
+        Saved_Free_Rice_PP = sh.getFloat("Saved_Free_Rice_PP",0);
+        Saved_Free_Wheat_PP = sh.getFloat("Saved_Free_Wheat_PP",0);
+        Saved_Deduct_Free_Rice_PP = sh.getFloat("Saved_Deduct_Free_Rice_PP",0);
+        Saved_Deduct_Free_Wheat_PP = sh.getFloat("Saved_Deduct_Free_Wheat_PP",0);
+        Saved_Take_Rice_Price_Pkg = sh.getFloat("Saved_Take_Rice_Price_Pkg",0);
+        Saved_Take_Wheat_Price_Pkg = sh.getFloat("Saved_Take_Wheat_Price_Pkg",0);
+        Saved_Rice_Price_Pkg = sh.getFloat("Saved_Rice_Price_Pkg",0);
+        Saved_Wheat_Price_Pkg = sh.getFloat("Saved_Wheat_Price_Pkg",0);
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+
+
+    }
+
+
+
+    public void openSetting(){
+        Intent  intent = new Intent(this, Setting.class);
+        startActivity(intent);
     }
 
     @Override
@@ -40,6 +129,13 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         if (parent.getId() == R.id.spinnerTextSize) {
             String valueFromSpinner = parent.getItemAtPosition(position).toString();
             int j=Integer.parseInt(valueFromSpinner);
+
+            // save inputed spinner position to sharedpreferences
+            int userChoice = parent.getSelectedItemPosition();
+            SharedPreferences sharedPref = getSharedPreferences("Saved", 0);
+            SharedPreferences.Editor prefEditor = sharedPref.edit();
+            prefEditor.putInt("userChoiceSpinner", userChoice);
+            prefEditor.commit();
 
             CalculationAndUpdate2(j);
         }
@@ -129,36 +225,36 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         int Number_of_Person;
 
-        int Rice_PP = 4;
-        int Wheat_PP = 1;
-        int Deduct_Rice_PP = 1;
-        int Deduct_Wheat_PP = 0;
+        float Rice_PP = Saved_Rice_PP; // 4
+        float Wheat_PP = Saved_Wheat_PP; // 1
+        float Deduct_Rice_PP = Saved_Deduct_Rice_PP;//1;
+        float Deduct_Wheat_PP = Saved_Deduct_Wheat_PP;//0;
 
-        int Free_Rice_PP = 0;
-        int Free_Wheat_PP = 0;
-        int Deduct_Free_Rice_PP = 0;
-        int Deduct_Free_Wheat_PP = 0;
+        float Free_Rice_PP = Saved_Free_Rice_PP;//0;
+        float Free_Wheat_PP = Saved_Free_Wheat_PP;//0;
+        float Deduct_Free_Rice_PP = Saved_Deduct_Free_Rice_PP;//0;
+        float Deduct_Free_Wheat_PP = Saved_Deduct_Free_Wheat_PP;//0;
 
-        int Rice_Price_Pkg = 15;
-        int Wheat_Price_Pkg = 20;
+        float Rice_Price_Pkg = Saved_Rice_Price_Pkg;//15;
+        float Wheat_Price_Pkg = Saved_Wheat_Price_Pkg;//20;
 
-        int Take_Rice_Price_PKg = 0;
-        int Take_Wheat_Price_PKg = 0;
-        int Take_Price_Pkg = 0;
+        float Take_Rice_Price_PKg = Saved_Take_Rice_Price_Pkg;//0;
+        float Take_Wheat_Price_PKg = Saved_Take_Wheat_Price_Pkg;//0;
+        float Take_Price_Pkg = 0;
 
 
-        int Rice_Total;
-        int Wheat_Total;
-        int Money_To_Take_For_Rice;
-        int Money_To_Take_For_Wheat;
-        int Money_To_Take;
-        int Money_To_Give_For_Both;
-        int Money_To_Give_For_Rice_Only;
+        float Rice_Total;
+        float Wheat_Total;
+        float Money_To_Take_For_Rice;
+        float Money_To_Take_For_Wheat;
+        float Money_To_Take;
+        float Money_To_Give_For_Both;
+        float Money_To_Give_For_Rice_Only;
 
-        int Free_Rice_Total;
-        int Free_Wheat_Total;
-        int Paid_Rice_Total;
-        int Paid_Wheat_Total;
+        float Free_Rice_Total;
+        float Free_Wheat_Total;
+        float  Paid_Rice_Total;
+        float Paid_Wheat_Total;
 
         Number_of_Person = j;
 
--- app/src/main/java/com/example/rasancalci/Setting.java
+++ app/src/main/java/com/example/rasancalci/Setting.java
@@ -2,13 +2,273 @@ package com.example.rasancalci;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Setting extends AppCompatActivity {
 
+    private Button Save_Button;
+    ///////////////////////////////////////////////////////////////////////////
+
+    private TextInputLayout Edit_Rice_PP;
+    private TextView View_Rice_PP;
+
+    private TextInputLayout Edit_Wheat_PP;
+    private TextView View_Wheat_PP;
+
+    private TextInputLayout Edit_Deduct_Rice_PP;
+    private TextView View_Deduct_Rice_PP;
+
+    private TextInputLayout Edit_Deduct_Wheat_PP;
+    private TextView View_Deduct_Wheat_PP;
+
+    private TextInputLayout Edit_Free_Rice_PP;
+    private TextView View_Free_Rice_PP;
+
+    private TextInputLayout Edit_Free_Wheat_PP;
+    private TextView View_Free_Wheat_PP;
+
+    private TextInputLayout Edit_Deduct_Free_Rice_PP;
+    private TextView View_Deduct_Free_Rice_PP;
+
+    private TextInputLayout Edit_Deduct_Free_Wheat_PP;
+    private TextView View_Deduct_Free_Wheat_PP;
+
+    private TextInputLayout Edit_Take_Rice_Price_Pkg;
+    private TextView View_Take_Rice_Price_Pkg;
+
+    private TextInputLayout Edit_Take_Wheat_Price_Pkg;
+    private TextView View_Take_Wheat_Price_Pkg;
+
+    private TextInputLayout Edit_Rice_Price_Pkg;
+    private TextView View_Rice_Price_Pkg;
+
+    private TextInputLayout Edit_Wheat_Price_Pkg;
+    private TextView View_Wheat_Price_Pkg;
+
+    //////////////////////////////////////////////////////////////////////////
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        Save_Button = (Button) findViewById(R.id.Save_Button);
+        ///////////////////////////////////////////////////////////////
+
+        Edit_Rice_PP = findViewById(R.id.edit_Rice_PP);
+        View_Rice_PP = findViewById(R.id.view_Rice_PP);
+
+        Edit_Wheat_PP = findViewById(R.id.edit_Wheat_PP);
+        View_Wheat_PP = findViewById(R.id.view_Wheat_PP);
+
+        Edit_Deduct_Rice_PP = findViewById(R.id.edit_Deduct_Rice_PP);
+        View_Deduct_Rice_PP = findViewById(R.id.view_Deduct_Rice_PP);
+
+        Edit_Deduct_Wheat_PP = findViewById(R.id.edit_Deduct_Wheat_PP);
+        View_Deduct_Wheat_PP = findViewById(R.id.view_Deduct_Wheat_PP);
+
+        Edit_Free_Rice_PP = findViewById(R.id.edit_Free_Rice_PP);
+        View_Free_Rice_PP = findViewById(R.id.view_Free_Rice_PP);
+
+        Edit_Free_Wheat_PP = findViewById(R.id.edit_Free_Wheat_PP);
+        View_Free_Wheat_PP = findViewById(R.id.view_Free_Wheat_PP);
+
+        Edit_Deduct_Free_Rice_PP = findViewById(R.id.edit_Deduct_Free_Rice_PP);
+        View_Deduct_Free_Rice_PP = findViewById(R.id.view_Deduct_Free_Rice_PP);
+
+        Edit_Deduct_Free_Wheat_PP = findViewById(R.id.edit_Deduct_Free_Wheat_PP);
+        View_Deduct_Free_Wheat_PP = findViewById(R.id.view_Deduct_Free_Wheat_PP);
+
+        Edit_Take_Rice_Price_Pkg = findViewById(R.id.edit_Take_Rice_Price_Pkg);
+        View_Take_Rice_Price_Pkg = findViewById(R.id.view_Take_Rice_Price_Pkg);
+
+        Edit_Take_Wheat_Price_Pkg = findViewById(R.id.edit_Take_Wheat_Price_Pkg);
+        View_Take_Wheat_Price_Pkg = findViewById(R.id.view_Take_Wheat_Price_Pkg);
+
+        Edit_Rice_Price_Pkg = findViewById(R.id.edit_Rice_Price_Pkg);
+        View_Rice_Price_Pkg = findViewById(R.id.view_Rice_Price_Pkg);
+
+        Edit_Wheat_Price_Pkg = findViewById(R.id.edit_Wheat_Price_Pkg);
+        View_Wheat_Price_Pkg = findViewById(R.id.view_Wheat_Price_Pkg);
+
+
+
+        /////////////////////////////////////////////////////////////
+
+
+        readDataFromSharedPreferences();
+
+        Save_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref",MODE_PRIVATE);
+                SharedPreferences.Editor myEdit = sharedPreferences.edit();
+                ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+                if(!(Edit_Rice_PP.getEditText().getText().toString().isEmpty())) {
+
+                    View_Rice_PP.setText(Edit_Rice_PP.getEditText().getText().toString());
+                    float Rice_PP = Float.parseFloat(View_Rice_PP.getText().toString());
+                    myEdit.putFloat("Saved_Rice_PP",Rice_PP);
+
+                }
+
+                if(!(Edit_Wheat_PP.getEditText().getText().toString().isEmpty())) {
+
+                    View_Wheat_PP.setText(Edit_Wheat_PP.getEditText().getText().toString());
+                    float Wheat_PP = Float.parseFloat(View_Wheat_PP.getText().toString());
+                    myEdit.putFloat("Saved_Wheat_PP",Wheat_PP);
+
+                }
+
+                if(!(Edit_Deduct_Rice_PP.getEditText().getText().toString().isEmpty())) {
+
+                    View_Deduct_Rice_PP.setText(Edit_Deduct_Rice_PP.getEditText().getText().toString());
+                    float Deduct_Rice_PP = Float.parseFloat(View_Deduct_Rice_PP.getText().toString());
+                    myEdit.putFloat("Saved_Deduct_Rice_PP",Deduct_Rice_PP);
+
+                }
+
+                if(!(Edit_Deduct_Wheat_PP.getEditText().getText().toString().isEmpty())) {
+
+                    View_Deduct_Wheat_PP.setText(Edit_Deduct_Wheat_PP.getEditText().getText().toString());
+                    float Deduct_Wheat_PP = Float.parseFloat(View_Deduct_Wheat_PP.getText().toString());
+                    myEdit.putFloat("Saved_Deduct_Wheat_PP",Deduct_Wheat_PP);
+
+                }
+
+                if(!(Edit_Free_Rice_PP.getEditText().getText().toString().isEmpty())) {
+
+                    View_Free_Rice_PP.setText(Edit_Free_Rice_PP.getEditText().getText().toString());
+                    float Free_Rice_PP = Float.parseFloat(View_Free_Rice_PP.getText().toString());
+                    myEdit.putFloat("Saved_Free_Rice_PP",Free_Rice_PP);
+
+                }
+
+                if(!(Edit_Free_Wheat_PP.getEditText().getText().toString().isEmpty())) {
+
+                    View_Free_Wheat_PP.setText(Edit_Free_Wheat_PP.getEditText().getText().toString());
+                    float Free_Wheat_PP = Float.parseFloat(View_Free_Wheat_PP.getText().toString());
+                    myEdit.putFloat("Saved_Free_Wheat_PP",Free_Wheat_PP);
+
+                }
+
+                if(!(Edit_Deduct_Free_Rice_PP.getEditText().getText().toString().isEmpty())) {
+
+                    View_Deduct_Free_Rice_PP.setText(Edit_Deduct_Free_Rice_PP.getEditText().getText().toString());
+                    float Deduct_Free_Rice_PP = Float.parseFloat(View_Deduct_Free_Rice_PP.getText().toString());
+                    myEdit.putFloat("Saved_Deduct_Free_Rice_PP",Deduct_Free_Rice_PP);
+
+                }
+
+                if(!(Edit_Deduct_Free_Wheat_PP.getEditText().getText().toString().isEmpty())) {
+
+                    View_Deduct_Free_Wheat_PP.setText(Edit_Deduct_Free_Wheat_PP.getEditText().getText().toString());
+                    float Deduct_Free_Wheat_PP = Float.parseFloat(View_Deduct_Free_Wheat_PP.getText().toString());
+                    myEdit.putFloat("Saved_Deduct_Free_Wheat_PP",Deduct_Free_Wheat_PP);
+
+                }
+
+                if(!(Edit_Take_Rice_Price_Pkg.getEditText().getText().toString().isEmpty())) {
+
+                    View_Take_Rice_Price_Pkg.setText(Edit_Take_Rice_Price_Pkg.getEditText().getText().toString());
+                    float Take_Rice_Price_Pkg = Float.parseFloat(View_Take_Rice_Price_Pkg.getText().toString());
+                    myEdit.putFloat("Saved_Take_Rice_Price_Pkg",Take_Rice_Price_Pkg);
+
+                }
+
+                if(!(Edit_Take_Wheat_Price_Pkg.getEditText().getText().toString().isEmpty())) {
+
+                    View_Take_Wheat_Price_Pkg.setText(Edit_Take_Wheat_Price_Pkg.getEditText().getText().toString());
+                    float Take_Wheat_Price_Pkg = Float.parseFloat(View_Take_Wheat_Price_Pkg.getText().toString());
+                    myEdit.putFloat("Saved_Take_Wheat_Price_Pkg",Take_Wheat_Price_Pkg);
+
+                }
+
+                if(!(Edit_Rice_Price_Pkg.getEditText().getText().toString().isEmpty())) {
+
+                    View_Rice_Price_Pkg.setText(Edit_Rice_Price_Pkg.getEditText().getText().toString());
+                    float Rice_Price_Pkg = Float.parseFloat(View_Rice_Price_Pkg.getText().toString());
+                    myEdit.putFloat("Saved_Rice_Price_Pkg",Rice_Price_Pkg);
+
+                }
+
+                if(!(Edit_Wheat_Price_Pkg.getEditText().getText().toString().isEmpty())) {
+
+                    View_Wheat_Price_Pkg.setText(Edit_Wheat_Price_Pkg.getEditText().getText().toString());
+                    float Wheat_Price_Pkg = Float.parseFloat(View_Wheat_Price_Pkg.getText().toString());
+                    myEdit.putFloat("Saved_Wheat_Price_Pkg",Wheat_Price_Pkg);
+
+                }
+
+
+
+                /////////////////////////////////////////////////////////////////////////////////////////////////////////
+                myEdit.commit();
+
+
+                openMainActivity();
+            }
+        });
+
+
+
+    }
+
+    public void readDataFromSharedPreferences(){
+        SharedPreferences sh = getSharedPreferences("MySharedPref",MODE_PRIVATE);
+        ////////////////////////////////////////////////////////////////////////////////////////
+
+        float Saved_Rice_PP = sh.getFloat("Saved_Rice_PP",0);
+        View_Rice_PP.setText(""+Saved_Rice_PP);
+
+        float Saved_Wheat_PP = sh.getFloat("Saved_Wheat_PP",0);
+        View_Wheat_PP.setText(""+Saved_Wheat_PP);
+
+        float Saved_Deduct_Rice_PP = sh.getFloat("Saved_Deduct_Rice_PP",0);
+        View_Deduct_Rice_PP.setText(""+Saved_Deduct_Rice_PP);
+
+        float Saved_Deduct_Wheat_PP = sh.getFloat("Saved_Deduct_Wheat_PP",0);
+        View_Deduct_Wheat_PP.setText(""+Saved_Deduct_Wheat_PP);
+
+        float Saved_Free_Rice_PP = sh.getFloat("Saved_Free_Rice_PP",0);
+        View_Free_Rice_PP.setText(""+Saved_Free_Rice_PP);
+
+        float Saved_Free_Wheat_PP = sh.getFloat("Saved_Free_Wheat_PP",0);
+        View_Free_Wheat_PP.setText(""+Saved_Free_Wheat_PP);
+
+        float Saved_Deduct_Free_Rice_PP = sh.getFloat("Saved_Deduct_Free_Rice_PP",0);
+        View_Deduct_Free_Rice_PP.setText(""+Saved_Deduct_Free_Rice_PP);
+
+        float Saved_Deduct_Free_Wheat_PP = sh.getFloat("Saved_Deduct_Free_Wheat_PP",0);
+        View_Deduct_Free_Wheat_PP.setText(""+Saved_Deduct_Free_Wheat_PP);
+
+        float Saved_Take_Rice_Price_Pkg = sh.getFloat("Saved_Take_Rice_Price_Pkg",0);
+        View_Take_Rice_Price_Pkg.setText(""+Saved_Take_Rice_Price_Pkg);
+
+        float Saved_Take_Wheat_Price_Pkg = sh.getFloat("Saved_Take_Wheat_Price_Pkg",0);
+        View_Take_Wheat_Price_Pkg.setText(""+Saved_Take_Wheat_Price_Pkg);
+
+        float Saved_Rice_Price_Pkg = sh.getFloat("Saved_Rice_Price_Pkg",0);
+        View_Rice_Price_Pkg.setText(""+Saved_Rice_Price_Pkg);
+
+        float Saved_Wheat_Price_Pkg = sh.getFloat("Saved_Wheat_Price_Pkg",0);
+        View_Wheat_Price_Pkg.setText(""+Saved_Wheat_Price_Pkg);
+
+        ///////////////////////////////////////////////////////////////////////////////////////
+
+    }
+
+
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
